be7adc8f67d726607478859bd919a0c5d270704c
Lizandro Franco <lizandrofranco@lizandros-mbp.attlocal.net>
2019-01-05 11:10:53 -0800

    addedd empty variables for scan use, added scan stuff, and have noun working

---
+++ src/madLibs/MadLibs.java
@@ -0,0 +1,26 @@
+package madLibs;
+import java.util.Scanner;
+public class MadLibs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String noun = "";
+		String adj = "";
+		String adj2 = "";
+		String color = "";
+		String animal = "";
+		System.out.println("give me a noun");
+		noun = scan.nextLine();
+		System.out.print(noun);
+		System.out.println("give me a adj");
+		
+		System.out.println("give me a adj2");
+		
+		System.out.println("give me a color");
+		
+		System.out.println("give me a animal");
+		
+	}
+
+}
